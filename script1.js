e176a9c37a50a6fb007dc4ccf9e5ab6cab8e42aa
Jayden Skuladottir <sask5403@colorado.edu>
2023-10-17 17:11:49 -0600

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,10 +1,8 @@
 // import { hello as helloStr, year, printTheYear } from "./script2.js";
 import * as script2Stuff from "./script2.js";
-import printTheMonth from "./script3.js";
+import printMyMajor from "./script3.js";
 
 console.log(script2Stuff.hello);
-console.log(script2Stuff.year);
-script2Stuff.printTheYear();
 
-printTheMonth();
+printMyMajor();
 
